b3199baca9a4a86f8194646fe73afb040bb8e533
LeonidasVS <villafrancosibrian@gmail.com>
2025-09-18 10:04:14 -0600

    Primer IU de la agenda

---
+++ app/src/main/java/com/example/crud_kotlin/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.crud_kotlin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.crud_kotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnIniciar.setOnClickListener {
+            startActivity(Intent(applicationContext,LoginActivity::class.java))
+        }
+
+        binding.btnRegistrar.setOnClickListener {
+            startActivity(Intent(applicationContext,RegistrarActivity::class.java))
+        }
+
+    }
+}
