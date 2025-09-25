4052026809a9a7a03a8d1891a167ecaaafeaadfb
LeonidasVS <villafrancosibrian@gmail.com>
2025-09-24 21:58:11 -0600

    Login activo

--- app/src/main/java/com/example/crud_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/crud_kotlin/MainActivity.kt
@@ -7,15 +7,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.crud_kotlin.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding=ActivityMainBinding.inflate(layoutInflater)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        if (firebaseAuth.currentUser != null){
+            irDashboard()
+        }
+
         enableEdgeToEdge()
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -25,12 +33,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnLogin.setOnClickListener {
-            startActivity(Intent(applicationContext,LoginActivity::class.java))
+
+            if (firebaseAuth.currentUser != null){
+                irDashboard()
+            }
+            else{
+                startActivity(Intent(applicationContext,LoginActivity::class.java))
+            }
         }
 
         binding.btnRegistrar.setOnClickListener {
             startActivity(Intent(applicationContext,RegistrarActivity::class.java))
         }
 
+    }
+
+    private fun irDashboard(){
+        startActivity(Intent(applicationContext, DashboardActivity::class.java))
     }
 }
